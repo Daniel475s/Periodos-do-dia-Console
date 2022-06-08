3bafbefdf57923af1a846732ddc0a5a5ef336cf9
Daniel Soares <87248276+Daniel475s@users.noreply.github.com>
2022-06-07 22:20:26 -0300

    Add files via upload

---
+++ ap1/ap1/Mensagem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ap1
+{
+    class Mensagem
+    {
+
+        #region Métodos Void
+        public void MensagemBomDia()
+        {
+            Console.WriteLine("Bom dia!");
+        }
+
+        public void MensagemBoaTarde()
+        {
+            Console.WriteLine("Boa Tarde!");
+
+        }
+
+        public void MensagemBoaNoite()
+        {
+            Console.WriteLine("Boa Noite!");
+
+        }
+
+        public void MensagemAlmoco()
+        {
+            Console.WriteLine("Bom Almoço!");
+
+        }
+
+        #endregion
+
+        #region Métodos Com Return
+
+        public static string Bomdia()
+        {
+            string mensagem = "Bom dia";
+            return mensagem;
+        }
+
+        public static string BoaTarde()
+        {
+            return "Boa Tarde";
+        }
+
+        public static string BoaNoite()
+        {
+            return "Boa Noite";
+        }
+
+        #endregion
+
+    }
+}
---
+++ ap1/ap1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apresentacao();
+            string periodo = PegaValorDoUsuario();
+
+            Validacao(periodo);
+
+            Console.ReadLine();
+        }
+
+        public static void Apresentacao()
+        {
+            Console.WriteLine("Olá! Manhã = M, Tarde = T, Noite = N, Almoço = A Selecione em qual período do dia estamos no momento: ");
+        }
+
+        public static string PegaValorDoUsuario()
+        {
+            string valorInseridoPeloUsuario = Console.ReadLine();
+
+            return valorInseridoPeloUsuario;
+        }
+
+        public static void Validacao(string p_periodo)
+        {
+            Mensagem msg = new Mensagem();
+
+
+            if (p_periodo == "M")
+            {
+                msg.MensagemBomDia();
+            }
+            else if (p_periodo == "T")
+            {
+                msg.MensagemBoaTarde();
+            }
+            else if (p_periodo == "N")
+            {
+                msg.MensagemBoaNoite();
+            }
+            else if (p_periodo == "A")
+            {
+                msg.MensagemAlmoco();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Período não informado corretamente.");
+            }
+
+        }
+
+    }
+
+
+
+}
+
